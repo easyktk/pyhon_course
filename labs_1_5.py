89dadea3e2755f2aad29da77abd9788ce5c0319c
easyktk <aNer11x@gmail.com>
2024-09-13 19:01:52 +0300

    few labs

--- labs_1_5.py
+++ labs_1_5.py
@@ -7,6 +7,9 @@ import random
 from lab_lib import Student, Lab
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+import re
+from faker import Faker
+
 
 class Lab1(Lab):
     @staticmethod
@@ -18,26 +21,26 @@ class Lab1(Lab):
         except ValueError:
             print("ValueError")
             return
-        res = 1/4-1/4*(math.sin((5/2*math.pi) - 8*v))
+        res = 1 / 4 - 1 / 4 * (math.sin((5 / 2 * math.pi) - 8 * v))
         print(f"1/4-1/4*(math.sin((5/2*math.pi) - 8*v)) = {res:.3f}")
         return res
 
     @staticmethod
-    def part2(value=None):
+    def part2():
         v = [input("Введіть а:"), input("Введіть b:"), input("Введіть c:")]
         v = map(int, v)
         try:
-           a, b, c = v
+            a, b, c = v
         except ValueError:
             print("ValueError")
             return
-        p = (a + b + c)/2
-        s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+        p = (a + b + c) / 2
+        s = math.sqrt(p * (p - a) * (p - b) * (p - c))
         print(f"Площа трикутника дорівнює {s:.3f}")
         return s
 
-    def part3(self, value=None):
-        value =input("Введіть шестизначний номер:")
+    def part3(self):
+        value = input("Введіть шестизначний номер:")
         if self._validate_input_for_digits(value) or len(value) != 6:
             print("Невірний номер")
             return
@@ -52,11 +55,11 @@ class Lab1(Lab):
             print("Невірний номер")
             return
         value = int(value)
-        print(f"{value//60} годин {value % 60} хвилин")
+        print(f"{value // 60} годин {value % 60} хвилин")
 
     def part5(self, value=None):
         if value is None:
-            value = input("birthd у форматі день-місяць-рік")
+            value = input("birth у форматі день-місяць-рік")
         try:
             day, month, year = map(int, (value.split("-")))
             date = datetime.date(year, month, day)
@@ -76,9 +79,9 @@ class Lab1(Lab):
             return
         value = int(value)
         if scale == "C":
-            print(value*(9/5)+32)
+            print(value * (9 / 5) + 32)
             return
-        print((value - 32)*(5/9))
+        print((value - 32) * (5 / 9))
 
     def practice_2(self, value=None):
         if value is None:
@@ -89,7 +92,7 @@ class Lab1(Lab):
         value = int(value)
         km_h = value * 3600 / 1000
         m_s = value / 3600 * 1000
-        print(f"{value } кілометрів за годину (км/год) = {m_s} метри за секунду (м/с)",
+        print(f"{value} кілометрів за годину (км/год) = {m_s} метри за секунду (м/с)",
               f"{value} метри за секунду (м/с) = {km_h} кілометрів за годину (км/год)")
 
     def practice_3(self, value1=None, value2=None):
@@ -104,10 +107,9 @@ class Lab1(Lab):
         except ValueError:
             print("Невірний ввід")
 
-
     def practice_4(self, value=None):
         if not value:
-            value = input("Введіть верню межу діапазону")
+            value = input("Введіть верхню межу діапазону")
         if self._validate_input_for_digits(value):
             print("Невірний ввід")
             return
@@ -129,8 +131,8 @@ class Lab1(Lab):
             print("Невірний ввід")
             return
         value = int(value)
-        print(f"кількість повних упаковок, які можна заповнити цими яблуками = {value//box_size}, "
-              f" кількість яблук, які залишаться поза упаковками = {value%box_size}")
+        print(f"кількість повних упаковок, які можна заповнити цими яблуками = {value // box_size}, "
+              f" кількість яблук, які залишаться поза упаковками = {value % box_size}")
 
 
 class Lab2(Lab):
@@ -142,7 +144,7 @@ class Lab2(Lab):
         except ValueError:
             print("Невірний ввід")
             return
-        print(abs(x) + abs(y)/2 <= 1)
+        print(abs(x) + abs(y) / 2 <= 1)
 
     def part2(self):
         number = input("Введіть чотиризначне число:")
@@ -155,8 +157,8 @@ class Lab2(Lab):
             return
 
         print(f'Число {"не " if "5" not in number else ""}містить цифру 5')
-        print(f'складається лише з {"не" if all(map(lambda x: int(x) % 2 != 0,  number)) else ""}парних чисел;')
-        print(f'сума його цифр {"більша" if sum(map(int,number)) > 10 else "менша"} за число 10;')
+        print(f'складається лише з {"не" if all(map(lambda x: int(x) % 2 != 0, number)) else ""}парних чисел;')
+        print(f'сума його цифр {"більша" if sum(map(int, number)) > 10 else "менша"} за число 10;')
 
         s = sum(map(int, number[0:2])) > sum(map(int, number[2:]))
         print(f"сума його перших двох цифр {"більша" if s else "не більша"} за суму наступних двох цифр")
@@ -174,16 +176,16 @@ class Lab2(Lab):
         perimetr = sum(sides)
         if c >= b + a:
             print("трикутника з такими сторонами не існує.")
-        elif c**2 == b**2 + a**2:
+        elif c ** 2 == b ** 2 + a ** 2:
             print("прямокутний трикутник")
-        elif c > b**2 + a**2:
+        elif c > b ** 2 + a ** 2:
             print("тупокутний трикутник")
-        elif c > b**2 + a**2:
+        elif c > b ** 2 + a ** 2:
             print("гострокутний трикутник;")
 
     @staticmethod
     def part4():
-        board = [["*"for i in range(8)] for j in range(8)]
+        board = [["*" for _ in range(8)] for j in range(8)]
         moves = input("Приклад вхідних і вихідних даних 1 1 5 5     :").split()
         try:
             moves = list(map(int, moves))
@@ -207,14 +209,14 @@ class Lab2(Lab):
         sum_for = 0
         for j in range(1, 11):
             for i in range(1, 6):
-                sum_for += (i**2 * x**i + 1)/(math.factorial(j) * i**2)
+                sum_for += (i ** 2 * x ** i + 1) / (math.factorial(j) * i ** 2)
 
         sum_while = 0
         j = 1
         while j <= 10:
             i = 1
             while i <= 5:
-                sum_while += (i**2 * x**i + 1)/(math.factorial(j) * i**2)
+                sum_while += (i ** 2 * x ** i + 1) / (math.factorial(j) * i ** 2)
                 i += 1
             j += 1
 
@@ -234,7 +236,7 @@ class Lab2(Lab):
         except ValueError:
             print("Невірний ввід")
             return
-        a1, b1, c1,  = first_sides
+        a1, b1, c1, = first_sides
         a2, b2, c2, = second_sides
         if first_sides == second_sides:
             print("коробки однакові")
@@ -270,14 +272,14 @@ class Lab2(Lab):
             return
         n = int(n)
         result = 1
-        for i in range(1, n+1, 2):
+        for i in range(1, n + 1, 2):
             result *= i
         print(result)
 
     @staticmethod
     def practice4():
         for i in range(0, 11, 1):
-            print(f"sin({i/10}) = {math.sin(i/10):.3f}")
+            print(f"sin({i / 10}) = {math.sin(i / 10):.3f}")
 
     def practice5(self):
         n = input("введіть n:")
@@ -296,21 +298,11 @@ class Lab2(Lab):
             median = sequence[len(sequence) // 2]
         print(f"медіана = {np.median(sequence)}")
         sigma = np.std(sequence)
-        s_arif = sum(sequence)/len(sequence)
-        result = [i for i in sequence if 3*sigma + s_arif > i < 3*sigma - s_arif]
+        s_arif = sum(sequence) / len(sequence)
+        result = [i for i in sequence if 3 * sigma + s_arif > i < 3 * sigma - s_arif]
         if result:
             print(f" елементи, які виходять за межі правила 3 сігм (правило 3-sigma rule):{result}")
 
-
-
-    def __hash__(self):
-        return hash((self.name, self.middle_name, self.surname, self.gender, self.age, self.course))
-
-    def __eq__(self, other):
-        if isinstance(other, Student):
-            if self.__hash__() == other.__hash__():
-                return True
-        return False
 
 class Lab3(Lab):
 
@@ -326,9 +318,9 @@ class Lab3(Lab):
             sequence = [random.randint(1, 9) for _ in range(100)]
         count = Counter(sequence)
         print(count)
-        print(f"діапазон [1, 3] = {sum(count[i] for i in range(1,4))},"
-                f"[4, 6] = {sum(count[i] for i in range(4,7))}, "
-              f"[7, 9] = {sum(count[i] for i in range(7,10))}")
+        print(f"діапазон [1, 3] = {sum(count[i] for i in range(1, 4))},"
+              f"[4, 6] = {sum(count[i] for i in range(4, 7))}, "
+              f"[7, 9] = {sum(count[i] for i in range(7, 10))}")
 
     def part3(self, sequence=None):
         if sequence is None:
@@ -338,7 +330,7 @@ class Lab3(Lab):
         one_more_sequence = sorted(sequence)
         one_more_sequence = one_more_sequence[:3] + [11111] + one_more_sequence[3:]
         print(one_more_sequence)
-        one_more_sequence = list(map(lambda x: x*3 if x > 10 else x, one_more_sequence))
+        one_more_sequence = list(map(lambda x: x * 3 if x > 10 else x, one_more_sequence))
         print(one_more_sequence)
         print(sequence)
 
@@ -360,22 +352,21 @@ class Lab3(Lab):
         ages = [student.age for student in sequence]
         age, count = Counter(ages).most_common(1)[0]
         ind = 0
-        #small chase of ValueError
+        # small chase of ValueError
         for i in range(count):
             index = ages.index(age, ind)
             student = sequence[index]
             ind = index + 1
             print(student.name, student.middle_name[0] + ".", student.surname[0] + ".")
 
-
     def part5(self):
         sequence = "".join([str(random.randint(0, 1)) for _ in range(100)])
         res = None
         for i in range(1, len(sequence)):
-            indexes = sequence.find(i*"1")
+            indexes = sequence.find(i * "1")
             if indexes == -1:
                 break
-            res = i*"1", indexes, indexes + i
+            res = i * "1", indexes, indexes + i
         print(res)
 
     def practice1(self):
@@ -392,7 +383,7 @@ class Lab3(Lab):
                 point_list = []
                 counter = 0
                 for c_x, c_y in zip(coords_x, coords_y):
-                    if (x - c_x)**2 + (y - c_y)**2 <= radius**2:
+                    if (x - c_x) ** 2 + (y - c_y) ** 2 <= radius ** 2:
                         counter += 1
                         point_list.append((c_x, c_y))
                 if counter > max_counter:
@@ -400,39 +391,67 @@ class Lab3(Lab):
                     max_coords = x, y
                     max_points_list = point_list
 
-
         circle = Circle(max_coords, radius, fill=False, label='circle')
         fig, sp = plt.subplots()
         sp.add_patch(circle)
         sp.scatter(coords_x, coords_y)
         sp.grid(True)
         print(max_coords, max_counter, max_points_list)
-
         plt.show()
 
+    def practice2(self, original_sentence=None):
+        if original_sentence is None:
+            original_sentence = \
+                ("Installing collected packages: pyparsing, pillow, packaging kiwisolver, fonttools, cycler, "
+                 "contourpy, matplotlib")
+        sentence = re.sub(r"[,:!?]", '', original_sentence)
+        sentence = sentence.split()
+        sentence.sort(key=len)
+        print(f"найкоротше слово речення{sentence[0], original_sentence.find(sentence[0]), len(sentence[0])}")
+        print(f"найдовше слово речення {sentence[-1], original_sentence.find(sentence[-1]), len(sentence[-1])}")
+        volves = []
+        for i in sentence:
+            word = []
+            for j in i:
+                if j in "AEIOUaeiou":
+                    word.append(j)
+            volves.append(word)
+        position = volves.index(sorted(volves)[-1])
+        print(f"слово, що містить найбільшу кількість голосних літер:{sentence[position]}")
 
+    def practice3(self, c=None):
+        # it ON**2 i gues it is better solution
+        if c is None:
+            fake = Faker('uk_UA')
+            addresses = [fake.address() for _ in range(10)]
+            c = [[fake.last_name(), fake.city(), random.choice(addresses)] for i in range(15)]
 
-
-
-
-
-
-
-
+        result = []
+        for index, data1 in enumerate(c):
+            for index1, data2 in enumerate(c):
+                _, city, address = data1
+                __, city1, address1 = data2
+                if city != city1 and address1 == address and index != index1:
+                    if ((__, city1, address1), (_, city, address)) not in result:
+                        result.append(((_, city, address), (__, city1, address1)))
+        for line in result:
+            print(line)
 
 
 class Lab4(Lab):
     pass
+
+
 class Lab5(Lab):
     pass
+
 
 class Lab6(Lab):
     pass
 
 
-
 if __name__ == "__main__":
     pass
-    #Lab2()()
+    # Lab2()()
     lab3 = Lab3()
-    lab3.practice1()
+    lab3.practice3()
