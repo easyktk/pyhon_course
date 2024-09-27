0b18206f0478e0dc55a4e59e1a8213389bf10169
easyktk <aNer11x@gmail.com>
2024-09-27 08:27:05 +0300

    commit

--- cisco_pe/progs/lab.py
+++ cisco_pe/progs/lab.py
@@ -10,13 +10,34 @@ digits = ['1111110',  # 0
           '1111011',  # 9
           ]
 
+x="""
+  # ### ### # # ### ### ### ### ### ###
+  #   #   # # # #   #     # # # # # # # 
+  # ### ### ### ### ###   # ### ### # # 
+  # #     #   #   # # #   # # #   # # # 
+  # ### ###   # ### ###   # ### ### ###
+"""
 
+# nums = [x[i:i+30] for i in range(0, len(x), 30)]
+nums = x.split("\n")
+nums = nums[1:-1]
+
+for ind, line in enumerate(nums):
+    nums[ind] = [line[i: i+3] for i in range(0,len(line), 4)]
+
+#print(nums)
 def print_number(num):
-    for i in num:
-        break
+    for line in nums:
+        res = ""
+        for i in num:
+            if int(i) == 0:
+                res += line[-1] + " "
+                continue
+            res += line[int(i) - 1] + " "
+        print(res)
 
 
 
+print_number("1234564546545454654654848")
 
-
-print_number(int(input("Enter the number you wish to display: ")))
+# print_number(int(input("Enter the number you wish to display: ")))
--- tst.py
+++ tst.py
@@ -1,1 +1,2 @@
-print('lam bda'.isalnum())
+print({i//2 : i**2 for i in range(10) if i % 2})
+print(1//2)
